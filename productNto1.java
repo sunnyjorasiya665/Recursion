b8a2534b9f46a8b87213092bf7d9c0719ca5ac59
SUNNY JORASIYA <sjorasiya665@gmail.com>
2023-10-06 22:26:11 +0530

    these are the code of recursion

---
+++ productNto1.java
@@ -0,0 +1,15 @@
+public class productNto1 {
+    public static void main(String[] args) {
+        int ans = factorial(5);
+        System.out.println(ans);
+
+    }
+
+    static int factorial(int n) {
+        if (n <= 1) {
+            return 1;
+        }
+        return n * factorial(n - 1);
+
+    }
+}
