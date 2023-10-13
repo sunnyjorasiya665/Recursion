d70c076f02b17d6ce8e2ce2baa9d7b5dfd8727e2
SUNNY JORASIYA <sjorasiya665@gmail.com>
2023-10-13 23:22:28 +0530

    sorting and pattern using recursion
    Merge sort using recursion
    
    merge Sort

---
+++ MergeSort.java
@@ -0,0 +1,51 @@
+import java.util.Arrays;
+
+public class MergeSort {
+    public static void main(String[] args) {
+        int[] arr = { 9, 8, 7, 6, 5, 4, 3, 2, 1 };
+        arr = mergesort(arr);
+        System.out.println(Arrays.toString(arr));
+    }
+
+    static int[] mergesort(int[] arr) {
+        if (arr.length == 1) {
+            return arr;
+        }
+        int mid = arr.length / 2;
+
+        int[] left = mergesort(Arrays.copyOfRange(arr, 0, mid));
+        int[] right = mergesort(Arrays.copyOfRange(arr, mid, arr.length));
+        return merge(left, right);
+    }
+
+    private static int[] merge(int[] left, int[] right) {
+        int[] mix = new int[left.length + right.length];
+        int i = 0;
+        int j = 0;
+        int k = 0;
+
+        while (i < left.length && j < right.length) {
+            if (left[i] <= right[j]) {
+                mix[k] = left[i];
+                i++;
+            } else {
+                mix[k] = right[j];
+                j++;
+            }
+            k++;
+        }
+
+        // if elements remains in any of the array
+        while (i < left.length) {
+            mix[k] = left[i];
+            i++;
+            k++;
+        }
+        while (j < right.length) {
+            mix[k] = right[j];
+            j++;
+            k++;
+        }
+        return mix;
+    }
+}
